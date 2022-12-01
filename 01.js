7312c2e05528205476445899461e135bd1164bc7
Peter Sliacky <peter.sliacky@gmail.com>
2022-12-01 18:55:09 +0100

    Fix output format

--- 01.js
+++ 01.js
@@ -4,4 +4,4 @@ const input = readFile('input/01-1.txt').split('\n\n')
 
 const result = input.map(x => x.split('\n').reduce((acc, x) => (acc + (parseInt(x) || 0)), 0))
 console.log(result, '\n\nMax energy: ', Math.max(...result))
-console.log('\n\nTop 3 energy: ', result.sort((x,y) => y - x).slice(0,3).reduce((acc, x) => (acc + x), 0))
+console.log('Top 3 energies: ', result.sort((x,y) => y - x).slice(0,3).reduce((acc, x) => (acc + x), 0))
