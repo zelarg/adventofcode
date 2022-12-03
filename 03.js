b8dd4e47fd71e6753f51f1a723f71d3da95dcd17
Peter Sliacky <peter.sliacky@gmail.com>
2022-12-03 10:09:22 +0100

    Day 3

---
+++ 03.js
@@ -0,0 +1,19 @@
+const { readFile, sumAll } = require('./helpers/init')
+const input = readFile('input/03-1.txt').split('\n').slice(0, -1)
+// ---------------------------------------------
+
+let r = input.map(x=> {
+    let a = x.slice(0, x.length/2)
+    let b = x.slice(x.length/2)
+    let c = a.match(`[${b}]`)[0].charCodeAt(0)
+    return c - ((c >= 97)?96:38)
+})
+console.log(sumAll(r))
+
+r = input.reduce((acc, x, i) => ( ((i % 3 == 0) ? acc.push([x]) : acc[acc.length - 1].push(x)) && acc ), [])
+r = r.map( x=> {
+    let ab = x[1].match(new RegExp(`[${x[0]}]`,'g')).join('')
+    let c = x[2].match(new RegExp(`[${ab}]`), 'g').join().charCodeAt(0)
+    return c - ((c >= 97)?96:38)
+} )
+console.log(sumAll(r))
--- helpers/init.js
+++ helpers/init.js
@@ -4,6 +4,11 @@ const readFile = (fileName) => {
     return fs.readFileSync(fileName, 'utf8')
 }
 
+const sumAll = (arr) => {
+    return arr.reduce((acc,x) => (acc + x), 0)
+}
+
 module.exports = {
-    readFile
+    readFile,
+    sumAll
 }
