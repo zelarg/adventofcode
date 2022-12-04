7e196fb817071772cf5c2efe1b9aa2e0757c9039
Peter Sliacky <peter.sliacky@gmail.com>
2022-12-04 09:53:14 +0100

    Day 4

---
+++ 04.js
@@ -0,0 +1,15 @@
+const { readFile, sumAll } = require('./helpers/init')
+const input = readFile('input/04-1.txt').split('\n').slice(0, -1)
+// ---------------------------------------------
+
+let r = input.filter(x=> {
+    let [[a,b],[c,d]] = x.split(',').map(x=>(x.split('-')))
+    return (a-c)*(b-d)<=0
+})
+console.log(r.length)
+
+r = input.filter(x=> {
+    let [[a,b],[c,d]] = x.split(',').map(x=>(x.split('-')))
+    return (b-c)*(a-d)<=0
+})
+console.log(r.length)
